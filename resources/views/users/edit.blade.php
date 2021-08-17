2a7c0917e48b99a1410c97571c9b76ea70fb72af
andres085 <andresito.martinez1985@gmail.com>
2021-08-17 20:44:45 -0300

    Formulario de cambio de contraseña y logica funcionando

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -12,7 +12,7 @@
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">{{ __('Update User') }}
-                <a href="{{route('users.index')}}" class="float-right">Volver</a>
+                    <a href="{{route('users.index')}}" class="float-right">Volver</a>
                 </div>
                 <div class="card-body">
                     <form method="POST" action="{{ route('users.update', $user->id) }}">
@@ -89,8 +89,46 @@
                     </form>
                 </div>
             </div>
-            
         </div>
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Change Password') }}</div>
+                    <div class="card-body">
+                        <form method="POST" action="{{ route('users.change.password', $user->id) }}">
+                            @csrf
+                            <div class="form-group row">
+                                <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+
+                                <div class="col-md-6">
+                                        <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+
+                                        @error('password')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                        @enderror
+                                </div>
+                            </div>
+
+                            <div class="form-group row">
+                                <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
+
+                                <div class="col-md-6">
+                                    <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                                </div>
+                            </div>
+
+                            <div class="form-group row mb-0">
+                                <div class="col-md-6 offset-md-4">
+                                    <button type="submit" class="btn btn-primary">
+                                        {{ __('Cambiar Password') }}
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+             </div>
+         </div>
     </div>
 </div>
 
